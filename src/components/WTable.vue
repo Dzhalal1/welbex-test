133c735d80bc2a9ff0da9ab1f83fb9d0d117776c
dzhalal1 <djalal.2012@mail.ru>
2022-10-15 14:03:18 +0400

    верстка таблицы добвление эплай

--- src/components/WTable.vue
+++ src/components/WTable.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative p-0">
-      <table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
-        <thead class="text-sm text-gray-700 bg-gray-100">
-        <tr class="text-left">
-          <th v-for="col in header" :key="col.value">{{ col.title }}</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr class="text-left bg-white border-t hover:bg-gray-50" v-for="(row,index) in body" :key="index">
-          <td class="" v-for="col in header" :key="col.value">{{ row[col.value] }}</td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
+  <div class="w_table_main">
+    <table class="w_table table-auto whitespace-no-wrap table-striped">
+      <thead class="w_head">
+      <tr class="text-left">
+        <th v-for="col in header" :key="col.value">{{ col.title }}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr class="w_body_tr" v-for="(row,index) in body" :key="index">
+        <td v-for="col in header" :key="col.value">{{ row[col.value] }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
