653d188161baafd674165ab1ef6a6cd923371a22
dzhalal1 <djalal.2012@mail.ru>
2022-10-15 13:48:20 +0400

    верстка таблицы

--- src/components/WTable.vue
+++ src/components/WTable.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <table>
-      <thead>
-      <tr>
-        <th v-for="col in header" :key="col.value">{{ col.title }}</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(row,index) in body" :key="index">
-        <td v-for="col in header" :key="col.value">{{ row[col.value] }}</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative p-0">
+      <table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
+        <thead class="text-sm text-gray-700 bg-gray-100">
+        <tr class="text-left">
+          <th v-for="col in header" :key="col.value">{{ col.title }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="text-left bg-white border-t hover:bg-gray-50" v-for="(row,index) in body" :key="index">
+          <td class="" v-for="col in header" :key="col.value">{{ row[col.value] }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import tailwindCss from './assets/main.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  render: h => h(App)
+    store,
+    tailwindCss,
+    render: h => h(App)
 }).$mount('#app')
